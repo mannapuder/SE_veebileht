26418976b5ed4602c4a0cebb4ca3239ac5fa4aba
mannapuder <mindikkaveeleiolesiin@gmail.com>
2019-11-30 21:12:44 +0200

    kujundus, muudetud js

--- JS/magic8ball.js
+++ JS/magic8ball.js
@@ -1,17 +1,26 @@
 window.onload = () => {
     let tsitaadid = [
-        "Õige mõte, James",
-        "Toetame igatahes!"
+        "Õige mõte. - John",
+        "Toetame igatahes! - Jäneste kogu",
+        "Mida sa pobised, kas sa mõnda võtet ka oskad? - Tamagotchi tiimi liige",
+        "Ära muretse, küll ma homseks midagi välja mõtlen. - Lotte",
+        "Kas see ei ole mitte sport? Mina ei tohi sporti teha. - Bruno",
+        "Õnnetus tuleb. - Lotte",
+        "Midagi ei tule siin maailmas niisama. Tasuta hammasrattaid pole olemas. - Oskar",
+        "Sa võidad homme nagunii. Sa oled ju parim leiutaja meie külas. - Lotte",
+        "Isver, kui ebameeldiv! - James",
+        "Ema palus öelda, et sa ei kugistaks. - Lotte",
+        "Kas see ei ole mitte sport? Mina ei tohi sporti teha. - Bruno"
     ];
     let vastus = null;
     document.getElementById("vasta_lotte").onclick = () => {
         document.getElementById("küsimus").value = "";
-        if (vastus != null){
+        if (vastus != null) {
             document.getElementById("questions_wrapper").removeChild(vastus)
         }
         vastus = document.createElement("p");
         vastus.id = "vastus";
-        vastus.textContent = tsitaadid[Math.floor(Math.random()*tsitaadid.length)];
+        vastus.textContent = tsitaadid[Math.floor(Math.random() * tsitaadid.length)];
         document.getElementById("questions_wrapper").appendChild(vastus)
     }
 };
