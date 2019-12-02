0fb71dd14aa788827d4324705c7b45df728ff2bc
Kadi S <56999958+Sabasik@users.noreply.github.com>
2019-12-02 16:44:38 +0200

    Add files via upload

--- JS/viktoriin.js
+++ JS/viktoriin.js
@@ -99,7 +99,7 @@
             },
             correctAnswer: "c"
         }, {
-            question: "Mis oli Alberti ainuke sõber enne Lotte ja Brunoga sõbrunemist?",
+            question: "Kes oli Alberti ainuke sõber enne Lotte ja Brunoga sõbrunemist?",
             answers: {
                 a: "Roheline kurk",
                 b: "Oranž porgand",
@@ -113,7 +113,7 @@
                 b: "Jaak",
                 c: "Adalbert",
             },
-            correctAnswer: "b"
+            correctAnswer: "a"
         }, {
             question: "Mille leiutas Lotte vanavanaisa vend?",
             answers: {
@@ -139,11 +139,99 @@
             },
             correctAnswer: "a"
         }, {
-            question: "Mida oskas teha robot, mille ideed Adalbert Oskarilt varastada tahtis?",
+            question: "Kes leiutas moosi pannkookide peale?",
             answers: {
-                a: "Ringe joonistada",
-                b: "Sokke triikida",
-                c: "Muusikat järgi tantsida",
+                a: "Lotte ema Anna",
+                b: "Lotte vanaema",
+                c: "Lotte vanatädi",
+            },
+            correctAnswer: "a"
+        }, {
+            question: "Lõpeta lause: Lotte kurivaim, ...",
+            answers: {
+                a: "...selg läks paigast ära!",
+                b: "...ei tohi liiga palju sporti teha!",
+                c: "...poisid, tooge suhkurt!"
+            },
+            correctAnswer: "c"
+        }, {
+            question: "Kes ütles: 'Oled sina alles paksuke, täitsa pallike valmis!'?",
+            answers: {
+                a: "Ville",
+                b: "vihmauurija Voldemar",
+                c: "Fred"
+            },
+            correctAnswer: "b"
+        }, {
+            question: "Millises filmis öeldi: 'Ja mina mõtlesin, et te olete jooksja!'?",
+            answers: {
+                a: "Leiutajateküla Lotte",
+                b: "Lotte ja kuukivi saladus",
+                c: "Lotte ja kadunud lohed"
+            },
+            correctAnswer: "b"
+        }, {
+            question: "'Aga ma ju küpsetasin pirukaid kaasa!' - mis pirukatega on tegu?",
+            answers: {
+                a: "Porgandipirukas",
+                b: "Lihapirukas",
+                c: "Kapsapirukas"
+            },
+            correctAnswer: "c"
+        }, {
+            question: "Kust leidsid Lotte ja Tik punase kivi?",
+            answers: {
+                a: "Ville juurest",
+                b: "Fredi juurest",
+                c: "Pingviinide saarelt"
+            },
+            correctAnswer: "c"
+        }, {
+            question: "Lõpeta lause: 'On jah...'",
+            answers: {
+                a: "...kellukesed!",
+                b: "...porgand!",
+                c: "...kivikesed!"
+            },
+            correctAnswer: "b"
+        }, {
+            question: "Lõpeta: 'Kes mind hüüab? Juba tulen!...'",
+            answers: {
+                a: "...Siin ma olen!",
+                b: "...Mis vaja on?",
+                c: "...Tere!"
+            },
+            correctAnswer: "a"
+        }, {
+            question: "Matkatarkus number 5:",
+            answers: {
+                a: "Üks õige rännumees peab lihtsalt uskuma neid asju.",
+                b: "Üks õige rännumees ei lähe kunagi ilma vihmavarjuta reisile.",
+                c: "Merest püütud pannkoogid on kõige magusamad."
+            },
+            correctAnswer: "c"
+        }, {
+            question: "Lõpeta: 'Praegu on aeglane salm. Kohe tuleb kiirem salm, siis..'",
+            answers: {
+                a: "...saamegi minema hakata.",
+                b: "...jõuamegi kohale.",
+                c: "...sõidame kiiremini."
+            },
+            correctAnswer: "c"
+        }, {
+            question: "Esimene asi judos:",
+            answers: {
+                a: "Sina kukud õigesti.",
+                b: "Sina kummardad.",
+                c: "Sina võtad vastasest kinni."
+            },
+            correctAnswer: "b"
+        }, {
+            question: "Lõpeta: 'Kes suvel ei harjuta,...'",
+            answers: {
+                a: "...see ei jõua talvel sentimeetritki sõita.",
+                b: "...on talvel hädas.",
+                c: "...ei jaksa talvel uisutada."
             },
             correctAnswer: "a"
         }
